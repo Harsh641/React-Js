b80efcd77dd6ba16b32d33ba8a7242798fdacab3
Harsh <harshjoshi641@gmail.com>
2021-07-31 12:32:19 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Demo1 } from './MyComponents/Demo1';
+
+function demo(){
+  return Demo1
+}
 
 function App() {
+  const name = "Harsh";
+  const d = new Date();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Welcome, {name}</h1>
+    <h1>Date: {d}</h1>
+    <Demo1/>
+    </>
   );
 }
 
---
+++ src/MyComponents/Demo1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Demo1 = () => {
+    return (
+        <div>
+            <h1>Harsh</h1>
+        </div>
+    )
+}
